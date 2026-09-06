68d674fa7b301962cfa8b4c58c9a257071972e45
ganesh <gani25343@gmail.com>
2026-09-06 13:20:20 +0530

    Add files via upload

---
+++ varLenKey.py
@@ -0,0 +1,10 @@
+# def percent(**markes):
+#     total=0
+#     count=0
+#     for mark in markes:
+#         total+=markes[mark]
+#         count+=1
+#     percentage=(total/(count*100))*100
+#     return percentage    
+# percen=percent(m1=99,physics=85,DS=88,python=70,OS=89,CO=77)
+# print(f'The percentage is:{percen:.2f}')
