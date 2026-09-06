3a2d90b93bea33e9e2caf1bb3b89f1b5e2c634b6
ganesh <gani25343@gmail.com>
2026-09-06 11:55:14 +0530

    Add files via upload

---
+++ agecount.py
@@ -0,0 +1,56 @@
+'''Valid age count'''
+# def ageCount():
+#     count=0
+#     while True:
+#         age=int(input('Enter age:'))
+#         if age>120:
+#             continue
+#         elif age==-1:
+#             break
+#         elif age<1:
+#             continue
+#         else:
+#             count+=1
+#     print(f'valid age count :{count}')
+# ageCount()            
+
+
+'''sum numbers multiple of 5'''
+# def sumNum():
+#     sum=0
+#     while True:
+#         num=int(input('Enter a number:'))
+#         if num==0:
+#             break
+#         elif num%5==0:
+#             sum+=num
+#         else:
+#             continue
+#     print(f'The sum of 5 multipe numbers is ; {sum}')        
+# sumNum()    
+
+'''count uppercase letters '''
+# def countupcase(string):
+#     count=0
+#     for i in string:
+#         if 'A'<=i<='Z':
+#             count+=1
+#         else:
+#             continue
+#     print(f'The upper case letters in {string} is {count}')     
+
+# string=input('Enter a string:')
+# countupcase(string)       
+
+'''Total sale amount'''
+# def saleamount(sales):
+#     total=0
+#     for i in sales:
+#         if i==0:
+#             continue
+#         else:
+#             total+=i
+#     print(f'The total sale amount is :{total}')   
+# sales=list(map(int,input('Enter sales amount:').split()))   
+# saleamount(sales)
+
---
+++ nestedfor.py
@@ -0,0 +1,59 @@
+'''student markes '''
+# def studentmarks():
+#     students=[]
+#     studcount=int(input('Enter number of students:'))
+#     for i in range(1,studcount+1):
+#         markes=list(map(int,input(f'Enter student {i} markes:').split()))
+#         students.append(markes)
+#     for i in range(len(students)):
+#         sum=0
+#         for j in (students[i]):
+#             sum+=j
+#         print(f'student {i+1} total markes : {sum}')    
+
+# studentmarks()        
+
+'''Total sales'''
+# def salessum():
+#     prodects=[]
+#     pro=int(input('Enter number of prodects:'))
+#     for i in range(1,pro+1):
+#         sale=list(map(int,input(f'Enter product {i} sales:').split()))
+#         prodects.append(sale)
+#     for i in range(len(prodects)):
+#         sum=0
+#         for j in (prodects[i]):
+#             sum+=j
+#         print(f'student {i+1} total markes : {sum}')    
+
+# salessum()  
+
+'''Highest markes in stubects '''
+# def studentmarks():
+#     students=[]
+#     studcount=int(input('Enter number of students:'))
+#     for i in range(1,studcount+1):
+#         markes=list(map(int,input(f'Enter student {i} markes:').split()))
+#         students.append(markes)
+#     for i in range(len(students)):
+#         highest=0
+#         for j in (students[i]):
+#             if j>=highest:
+#                 highest=j
+#         print(f'student {i+1} highest markes : {highest}')    
+
+# studentmarks()  
+
+'''Theater seat booking count'''
+# def bookingcount():
+#     rows=5
+#     seats=6
+#     for i in range(1,rows+1):
+#         booked=0
+#         marks=list(map(int,input(f'Enter row {i} seat status:').split()))
+#         for j in range(seats):
+#             if marks[j]==1:
+#                 booked+=1
+#         print(f'Row {i} booked seats : {booked}')  
+# bookingcount()              
+        
